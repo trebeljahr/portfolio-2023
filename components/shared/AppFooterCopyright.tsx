b3c17e1e8adb25c948c4db275e7addd45473646e
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-03-07 17:33:04 +0100

    Remove stoman references

--- components/shared/AppFooterCopyright.tsx
+++ components/shared/AppFooterCopyright.tsx
@@ -2,19 +2,12 @@ function AppFooterCopyright() {
   return (
     <div className='flex items-center justify-center text-center font-general-regular'>
       <div className='text-lg text-ternary-dark dark:text-ternary-light'>
-        &copy; {new Date().getFullYear()}
+        &copy; {new Date().getFullYear()}.{' '}
         <a
-          href='https://github.com/realstoman/nextjs-tailwindcss-portfolio'
-          target='__blank'
-          className='ml-1 hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 duration-500'>
-          Next.js & Tailwind CSS Portfolio
-        </a>
-        .{' '}
-        <a
-          href='https://stoman.me'
+          href='https://trebeljahr.com'
           target='__blank'
           className='ml-1 font-medium uppercase text-secondary-dark dark:text-secondary-light hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 duration-500'>
-          Stoman
+          Trebeljahr
         </a>
       </div>
     </div>
